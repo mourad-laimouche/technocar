92ad6a5da9c50dd834293355c898d5321d90e422
mouradgaming <laimourad2009@gmail.com>
2025-06-21 00:05:36 +0100

    10

--- client/src/components/rental-calculator.tsx
+++ client/src/components/rental-calculator.tsx
@@ -115,8 +115,8 @@ export default function RentalCalculator({ car }: RentalCalculatorProps) {
   };
 
   const rawTotalPrice = calculateTotalPrice(days, car.name);
-  const totalPrice = Math.round(rawTotalPrice / 100) * 100; // Arrondi au multiple de 1 000 DA le plus proche
-  const currentPricePerDay = Math.round((totalPrice / days) / 100) * 100; // Arrondi au multiple de 1 000 DA
+  const totalPrice = Math.round(rawTotalPrice / 10) * 10; // Arrondi au multiple de 1 000 DA le plus proche
+  const currentPricePerDay = Math.round((totalPrice / days) / 10) * 10; // Arrondi au multiple de 1 000 DA
 
   // Fonction pour obtenir le badge de réduction
   const getDiscountBadge = (numberOfDays: number) => {
